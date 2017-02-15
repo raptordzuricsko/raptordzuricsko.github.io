8495d9b8ac0a0dd33cb6342847784d03dc24e3e5
Raptor Dz <gregdzuricsko@gmail.com>
2017-02-14 20:21:19 -0600

    refresh on how to use webpack and npm

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+     entry: './intro.js',
+     output: {
+         path: './bin',
+         filename: 'bundle.js',
+     },   module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    }
+
+ }
